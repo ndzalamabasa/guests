ecb43b2f647ad58c697d734245bb3f99651967fe
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2022-06-12 10:17:16 +0200

    Populated tables to be used in the main table

---
+++ sql-scripts/populate-tables.sql
@@ -0,0 +1,71 @@
+-- Invitors:
+INSERT INTO inviters (invitorName)
+VALUES('Mrs T.Ndambo'),
+('Mrs Dos antos'),
+('Matildah Pikiti'),
+('Linda Mtonga'),
+('Ms R.Ndambo'),
+('Mrs A.Masebo');
+
+-- Countries:
+INSERT INTO countries (countryName)
+VALUES('USA'),
+('Cape Town, RSA'),
+('Durban, RSA'),
+('RSA'),
+('Senegal'),
+('Kenya'),
+('Namibia'),
+('DRC'),
+('Ghana'),
+('UK'),
+('Dubai, UAE'),
+('Scotland'),
+('Choma, Zambia'),
+('Kabwe, Zambia'),
+('Ndola, Zambia'),
+('Petauke, Zambia'),
+('Livingstone'),
+('Lusaka, Zambia'),
+('Zambia'),
+('Zimbabwe'),
+('Bulgaria'),
+('Nigeria'),
+('Italy');
+
+INSERT INTO groups (groupName)
+VALUES('AUC Namibia'),
+('AUC Durban'),
+('AUC Cape Town'),
+('Events Manager/Comm Members'),
+('Directors'),
+("Chairman's Office"),
+('AU Commercial Investments'),
+('AU Food Security'),
+('AU Aviation'),
+('AU Health'),
+('AU Technogies'),
+('AU Admin'),
+('AU Commodity House'),
+('AU Travel'),
+('QPay'),
+('AU Consultants'),
+('RSA International Guests');
+
+INSERT INTO flights (commercialFlight)
+VALUES('USA-RSA-RTN'),
+('SENEGAL-RSA-RTN'),
+('KENYA-RSA-RTN'),
+('GHANA-RSA-RTN'),
+('DUBAI-RSA-RTN'),
+('UK-LUN-RTN'),
+('USA-LUN-RTN'),
+('SCOTLAND-LUN-RTN'),
+('LVI-JNB-OW'),
+('JNB-LVI-JNB-RTN'),
+('JNB-LVI-LUN-JNB'),
+('NYC-JNB-LVI-NYC'),
+('NYC-LVI-NYC'),
+('LUN-JNB-OW'),
+('NYC-LUN-NYC'),
+('NYC-DBX-JNB-NYC');
